3e42e724567c70e120816886e47847cf62f127b7
hakjun2 <143589379+hakjun2@users.noreply.github.com>
2024-01-05 16:19:15 +0900

    Title: 행복한 수열의 개수, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240105/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240105/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -20,26 +20,120 @@ int main() {
     int cnt = 0;
     for(int i = 0 ; i < n ; i++) {
         int cnt_same = 0;
+        int cnt_same2 = 0;
+        int same = 0;
+        int same2 = 0;
         for(int j = 0 ; j<n-1 ; j++) {
-            if(grid[i][j] == grid[i][j+1]) {
+            if(grid[i][j] == grid[i][j+1] && same == 0) {
                 cnt_same++;
-            }
-            if(cnt_same == m-1) {
+                same = grid[i][j];
+                if(cnt_same == m-1) {
                 cnt++;
                 break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i][j+1] && grid[i][j] == same) {
+                cnt_same++;
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i][j+1] && same2 == 0) {
+                cnt_same2++;
+                same2 = grid[i][j];
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i][j+1] && grid[i][j] == same2) {
+                cnt_same2++;
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
             }
         }
     }
     for(int j = 0 ; j < n ; j++) {
         int cnt_same = 0;
+        int cnt_same2 = 0;
+        int same = 0;
+        int same2 = 0;
         for(int i = 0 ; i<n-1 ; i++) {
-            if(grid[i][j] == grid[i+1][j]) {
+            
+            if(grid[i][j] == grid[i+1][j] && same == 0) {
                 cnt_same++;
-            }
-            if(cnt_same == m-1) {
+                same = grid[i][j];
+                if(cnt_same == m-1) {
                 cnt++;
                 break;
             }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i+1][j] && grid[i][j] == same) {
+                cnt_same++;
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i+1][j] && same2 == 0) {
+                cnt_same2++;
+                same2 = grid[i][j];
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            if(grid[i][j] == grid[i+1][j] && grid[i][j] == same2) {
+                cnt_same2++;
+                if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+            if(cnt_same2 == m-1) {
+                cnt++;
+                break;
+            }
+                continue;
+            }
+            
         }
     }
     cout << cnt;
