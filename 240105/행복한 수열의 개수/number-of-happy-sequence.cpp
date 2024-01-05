9e7d5869a66452a5840fa64291dc9f5dd705ae95
hakjun2 <143589379+hakjun2@users.noreply.github.com>
2024-01-05 15:43:18 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N 100
+
+int grid[MAX_N][MAX_N];
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0 ; i < n ; i++) {
+        for( int j = 0 ; j < n ; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    int cnt = 0;
+    for(int i = 0 ; i < n ; i++) {
+        int cnt_same = 0;
+        for(int j = 0 ; j<n-1 ; j++) {
+            if(grid[i][j] == grid[i][j+1]) {
+                cnt_same++;
+            }
+            if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+        }
+    }
+    for(int j = 0 ; j < n ; j++) {
+        int cnt_same = 0;
+        for(int i = 0 ; i<n-1 ; i++) {
+            if(grid[i][j] == grid[i+1][j]) {
+                cnt_same++;
+            }
+            if(cnt_same == m-1) {
+                cnt++;
+                break;
+            }
+        }
+    }
+    cout << cnt;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
