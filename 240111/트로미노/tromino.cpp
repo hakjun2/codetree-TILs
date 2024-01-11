c8480d310af7412295ae3eccc35162c5e3656616
hakjun2 <143589379+hakjun2@users.noreply.github.com>
2024-01-11 15:44:53 +0900

    Title: 트로미노, Time: 10ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240111/트로미노/tromino.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define MAX_N 1000
+int grid[MAX_N][MAX_N] = {};
+int n,m;
+int main() {
+
+    cin >> n >> m;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < m ; j++) {
+            cin >> grid[i][j];
+        }
+    }
+    int cnt = 0; int maxc = 0;
+    for(int i = 0 ; i < n ; i++) {
+        for(int j = 0 ; j < m ; j++) {
+            cnt = grid[i][j] + grid[i+1][j] + grid[i+1][j+1];
+            maxc = max(cnt, maxc);
+            cnt = grid[i][j+1] + grid[i+1][j+1];
+            maxc = max(cnt, maxc);
+            cnt = grid[i][j] + grid[i][j+1] + grid[i+1][j];
+            maxc = max(cnt, maxc);
+            cnt = grid[i][j] + grid[i][j+1] + grid[i+1][j+1];
+            maxc = max(cnt, maxc);
+            cnt = grid[i][j] + grid[i][j+1] + grid[i][j+2];
+            maxc = max(cnt, maxc);
+            cnt = grid[i][j] + grid[i+1][j] + grid[i+2][j];
+            maxc = max(cnt, maxc);
+        }
+    }
+    cout << maxc;
+    return 0;
+}
